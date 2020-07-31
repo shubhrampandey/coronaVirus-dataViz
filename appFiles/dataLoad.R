54f3dcc98480c2319e3c10009e40cdf90e6d9368
shubhrampandey <45200005+shubhrampandey@users.noreply.github.com>
2020-07-31 23:04:50 +0530

    update data

--- appFiles/dataLoad.R
+++ appFiles/dataLoad.R
@@ -9,7 +9,7 @@ names(coronavirus) = c("date","countryCode","region","lat","lon",
                        "Confirmed","Recovered","Deaths","countryName")
 
 #### New data
-# 
+# # 
 # df1 <- read.csv(file = "https://raw.githubusercontent.com/ulklc/covid19-timeseries/master/countryReport/raw/rawReport.csv", stringsAsFactors = F)
 # # #
 # write.csv(df1,"data/coronavirus1.csv")
